949c6a4cb190b40caaa79ff45fc0a03afc0cb686
Machiko Yasuda <machiko.yasuda@tofugu.com>
2021-05-21 14:50:13 -0700

    font colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ module.exports = {
     },
   },
   theme: {
+    colors: {
+      black: '#000',
+      gray: {
+        900: '#1F2021'
+      },
+      green: {
+        300: '#92B99D',
+        400: '#59966D',
+        500: '#74A882',
+        600: '#6AA078',
+        700: '#3A724F',
+        800: '#04472B',
+        900: '#02502F'
+      },
+      yellow: {
+        50: '#E4FFAD', // nav highlight
+        200: '#FFE67B', // button
+        500: '#E27B38' // ticker, button
+      },
+      white: '#fff'
+    },
     container: {
       center: true,
     },
