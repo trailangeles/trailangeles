4752a10d7bf3c4a8e881f4b7267f40bcc79477da
Maxwell Ogden <max@maxogden.com>
2023-04-06 15:50:14 -0700

    test caching again

--- src/sw.js
+++ src/sw.js
@@ -1,27 +1,36 @@
 let cacheName = 'offline-store'
 
-// self.addEventListener('install', (e) => {
-//   e.waitUntil(
-//     caches.open(cacheName).then((cache) => cache.addAll([
-//       '/static/css/style.css',
-//       '/static/js/alpine.js'
-//     ])),
-//   );
-// });
+var cache = async (req, res) => {
+  const cache = await caches.open(cacheName)
+  console.log(`[Service Worker] Caching new resource: ${req.url}`)
+  await cache.put(req, res.clone())
+  // var currentPage = req.referrer
+  // var cached = await cache.match(currentPage)
+  // if (!cached) {
+  //   console.log(`[Service Worker] Caching page: ${currentPage}`)
+  //   await cache.add(currentPage)
+  // }
+  return res;
+}
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })()
-  );
-});
+  var handler = async () => {
+    if (!e.request.url.startsWith('http')) {
+      console.log(`[Service Worker] Skipping: ${e.request.url}`)
+      return fetch(e.request)
+    }
+
+    let response
+    try {
+      response = await fetch(e.request)
+      await cache(e.request, response)
+      return response
+    } catch (err) {
+      console.log("Fetch error", err)
+      console.log(`[Service Worker] Serving from cache: ${e.request.url}`)
+      return caches.match(e.request)
+    }
+  }
+
+  e.respondWith(handler())
+})
